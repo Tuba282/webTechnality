62863efe4557b65f3a31908896c6af4e17b7ba1f
Tuba282 <tubajan282@gmail.com>
2025-10-28 21:44:45 +0500

    hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import React from "react";
 import { AnimatedLetters } from "./SplitTextGSAP";
 import HeroSwiper from "./HeroSwiper";
-import HeroMarquee from "./HeroMarquee";
 
 const Hero = () => {
   return (
@@ -14,21 +13,21 @@ const Hero = () => {
 
 
       {/* ---- Main Text ---- */}
-      <div className="md:w-[800px] sm:w-100  z-10 flex flex-wrap justify-between gap-5 md:gap-8 xl:gap-10 items-center my-4">
+      <div className="md:w-[750px] sm:w-100  z-10 flex flex-wrap justify-between gap-2 items-center my-4">
         <AnimatedLetters
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1.2, duration: 0.6 }}
           delay={1.2}
           text="Brands"
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-sans font-extrabold text-black"
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-[Mulish] font-semibold text-black mb:10 sm:mb-10"
         />
 
         <motion.div
           initial={{ scale: 0, opacity: 0, y: -50 }}
           animate={{ scale: 1, opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="-rotate-6 border-3 border-black bg-white/90 w-24 h-16 sm:w-32 sm:h-24 rounded-3xl md:rounded-4xl shadow-black shadow-md flex items-center justify-center"
+          className="-rotate-6 border-3 border-black bg-white/90 w-24 h-16 sm:w-32 sm:h-24 rounded-3xl md:rounded-4xl shadow-black shadow-md flex items-center justify-center mb:10 sm:mb-10"
         >
           <HeroSwiper />
         </motion.div>
@@ -37,7 +36,7 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1.5, duration: 0.6 }}
           delay={1.5}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans font-light text-[#737373]"
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans font-light text-[#737373] mb:10 sm:mb-10"
           text="Grow"
         />
         <AnimatedLetters
@@ -45,7 +44,7 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1.9, duration: 0.6 }}
           delay={1.9}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans font-extrabold text-black"
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans font-light text-[#737373]"
           text="Fast"
         />
         <motion.div
@@ -58,10 +57,10 @@ const Hero = () => {
           <marquee className="" behavior="smooth" direction="left">
             <div className="flex! items-center gap-x-20">
               
-              <span className="flex! items-center text-[#848484] text-4xl font-light"><img src="./framer.png" alt="" className="w-8 h-8 object-cover!" /> Framer Motion</span>
-              <span className="flex! items-center text-[#848484] text-4xl font-light"><img src="./webflow-icon.png" alt="" className="w-8 h-8 object-cover!" />Webflow</span>
-              <span className="flex! items-center text-[#848484] text-4xl font-light"><img src="./contra.png" alt="" className="w-8 h-8 object-cover!" />Contra</span>
-              <span className="flex! items-center text-[#848484] text-4xl font-light">Jitter</span>
+              <span className="flex! items-center text-[#848484] text-xl sm:text-4xl font-light"><img src="./framer.png" alt="" className="w-8 h-8 object-cover!" /> Framer Motion</span>
+              <span className="flex! items-center text-[#848484] text-xl sm:text-4xl font-light"><img src="./webflow-icon.png" alt="" className="w-8 h-8 object-cover!" />Webflow</span>
+              <span className="flex! items-center text-[#848484] text-xl sm:text-4xl font-light"><img src="./contra.png" alt="" className="w-8 h-8 object-cover!" />Contra</span>
+              <span className="flex! items-center text-[#848484] text-xl sm:text-4xl font-light">Jitter</span>
             </div>
             </marquee>
         </motion.div>
@@ -70,7 +69,7 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 2.2, }}
           delay={2.2}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans font-light text-[#737373]"
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-sans  font-semibold text-black "
           text="With us"
         />
 
