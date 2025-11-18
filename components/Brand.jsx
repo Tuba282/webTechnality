f68c0a659ab47e4e49366dc89dca612fd0df8765
Tuba282 <156935628+Bushrajan@users.noreply.github.com>
2025-11-19 01:12:43 +0500

    changes

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -27,113 +27,6 @@ const Brand = () => {
         })
     }
     return (
-<<<<<<< HEAD
-        <section className="relative w-full h-[65vh] flex items-center justify-center overflow-hidden">
-
-             {/* LEFT BADGES */}
-            <div className="absolute left-10 lg:left-20 xl:left-30 xxl:left-40 top-1/2 -translate-y-1/2 hidden xl:flex flex-col gap-6 items-end">
-
-                <motion.div 
-                    custom={0}
-                    variants={badgeVariantLeft}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 rounded-full rotate-6 backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-orange-500 p-1 rounded-full"> <MdWindow className='text-white text-md' /></span>
-                        <p className="font-medium">Design systems</p>
-                    </div>
-                </motion.div>
-                <motion.div 
-                    custom={1}
-                    variants={badgeVariantLeft}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 w-[150px] -rotate-3 rounded-full backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-[#464646] p-1 rounded-full"> <BiWindows className='text-white text-md' /></span>
-                        <p className="font-medium">UX Design</p>
-                    </div>
-                </motion.div>
-                <motion.div 
-                    custom={2}
-                    variants={badgeVariantLeft}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 w-[130px] -rotate-6 rounded-full backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-[#05A9FF] p-1 rounded-full"> <IoSearch className='text-white text-md' /></span>
-                        <p className="font-medium">Research</p>
-                    </div>
-                </motion.div>
-
-
-            </div>
-
-            {/* MAIN CENTER TEXT */}
-            <div className="text-center px-10 relative z-10">
-                <p className="text-gray-500 italic mb-4 tracking-wide">
-                    ——— Hello! ———
-                </p>
-
-                <h1 className="text-3xl sm:text-4xl md:text-5xl font-sans font-light leading-tight">
-                    We help brands grow with standout <br />
-                    design, clear branding, and content <br />
-                    that drives <span className="text-gray-400">results.</span>
-                </h1>
-            </div>
-
-            {/* RIGHT BADGES */}
-            <div className="absolute right-10 lg:right-20 xl:right-30 xxl:right-40 top-1/2 -translate-y-1/2 hidden xl:flex flex-col gap-6">
-
-                <motion.div 
-                    custom={0}
-                    variants={badgeVariantRight}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 rounded-full -rotate-6 backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-orange-500 p-1 rounded-full"> <MdOutlineAnimation className='text-white text-md' /></span>
-                        <p className="font-medium">Animation </p>
-                    </div>
-                </motion.div>
-                <motion.div 
-                    custom={1}
-                    variants={badgeVariantRight}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 w-[150px] rotate-3 rounded-full backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-[#464646] p-1 rounded-full"> <TbBrandArc className='text-white text-md' /></span>
-                        <p className="font-medium">Branding</p>
-                    </div>
-                </motion.div>
-                <motion.div 
-                    custom={2}
-                    variants={badgeVariantRight}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, margin: "-100px" }}
-                    className="px-2 py-2 w-[130px] rotate-6 rounded-full backdrop-blur-xs bg-black/10 border border-white/10 "
-                >
-                    <div className="ps-1 pe-3 py-1 flex items-center gap-2 bg-white shadow-md  rounded-full">
-                        <span className=" bg-[#05A9FF] p-1 rounded-full"> <TbBrandCtemplar className='text-white text-md' /></span>
-                        <p className="font-medium">Stratergy</p>
-                    </div>
-                </motion.div>
-            </div>
-
-=======
         <section className="relative w-full h-[80vh] flex items-center justify-center bg-[#f3f3f3] overflow-hidden">
 
             {/* Background light streaks */}
@@ -242,7 +135,6 @@ const Brand = () => {
                 </motion.div>
             </div>
 
->>>>>>> 45cb35248ac91638d4a87679067d180967251334
         </section>
     );
 }
