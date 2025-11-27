6d04a48871ed98371b258ca42f8a2095f779b888
Tuba282 <tubajan282@gmail.com>
2025-11-27 15:58:16 +0500

    row pages

---
+++ app/portfolio/page.js
@@ -0,0 +1,14 @@
+
+import Portfolio from '@/screens/Portfolio/Portfolio'
+import React from 'react'
+
+
+const PortfolioPage = () => {
+  return (
+    <div className='w-full'>
+      <Portfolio/>
+    </div>
+  )
+}
+
+export default PortfolioPage
